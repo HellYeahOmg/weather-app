1d5a2773e92933dee265ba660f224d499f5e1119
Vadim <vadim@iMac-Vadim.Dlink>
2018-11-08 20:44:39 +0300

    Updates

---
+++ precache-manifest.04498404b387c37e8dcd2c295e96aca0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "d43070d459963d3a283e",
+    "url": "/weather-app/static/js/main.d43070d4.chunk.js"
+  },
+  {
+    "revision": "04fc0ec075607a3afe1e",
+    "url": "/weather-app/static/js/1.04fc0ec0.chunk.js"
+  },
+  {
+    "revision": "d43070d459963d3a283e",
+    "url": "/weather-app/static/css/main.7009ed89.chunk.css"
+  },
+  {
+    "revision": "d293521d921415d57a0613891063f7dc",
+    "url": "/weather-app/index.html"
+  }
+];
